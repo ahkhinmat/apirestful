5e8d290319cb758dbea6795f89d10b3b7da23562
ta kha <ta.minhkha@gmail.com>
2021-03-30 14:28:07 +0700

    ok

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -1,23 +1,4 @@
 
-// const { checkToken } = require("../../auth/token_validation");
-
-// const {
-//   createUser,
-//   login,
-//   getUserByUserId,
-//   getUsers,
-//   updateUsers,
-//   deleteUser
-// } = require("./user.controller");
-// const router = require("express").Router();
-// router.get("/", getUsers);
-// router.post("/", createUser);
-// router.get("/:id", getUserByUserId);
-// router.post("/login", login);
-// router.patch("/", updateUsers);
-// router.delete("/", deleteUser);
-
-// module.exports = router;
 const router = require("express").Router();
 const { checkToken } = require("../../auth/token_validation");
 const {
